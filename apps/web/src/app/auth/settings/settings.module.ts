785430d963f9e58124978dba869f102b403ad3ba
Jake Fink <jfink@bitwarden.com>
2023-11-13 16:03:42 -0500

    use new key rotation service on change password component

--- apps/web/src/app/auth/settings/settings.module.ts
+++ apps/web/src/app/auth/settings/settings.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 
 import { PasswordCalloutComponent } from "@bitwarden/auth";
 
+import { AccountRecoveryModule } from "../../admin-console/organizations/members/services/account-recovery/account-recovery.module";
 import { SharedModule } from "../../shared";
 import { EmergencyAccessModule } from "../emergency-access";
+import { KeyRotationModule } from "../key-rotation/key-rotation.module";
 
 import { ChangePasswordComponent } from "./change-password.component";
 import { WebauthnLoginSettingsModule } from "./webauthn-login-settings";
@@ -14,6 +16,8 @@ import { WebauthnLoginSettingsModule } from "./webauthn-login-settings";
     WebauthnLoginSettingsModule,
     EmergencyAccessModule,
     PasswordCalloutComponent,
+    AccountRecoveryModule,
+    KeyRotationModule,
   ],
   declarations: [ChangePasswordComponent],
   providers: [],
