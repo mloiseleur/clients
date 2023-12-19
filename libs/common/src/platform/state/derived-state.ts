02209b5055149297ab753e4b32712ae4673a339e
Matt Gibson <mgibson@bitwarden.com>
2023-12-19 18:33:52 -0500

    remove implementation notes

--- libs/common/src/platform/state/derived-state.ts
+++ libs/common/src/platform/state/derived-state.ts
@@ -6,9 +6,3 @@ export interface DerivedState<T> {
   state$: Observable<T>;
   forceValue(value: T): Promise<T>;
 }
-
-// Needs to support dependent services
-// Needs to switchMap between emissions
-// Needs to skip work until being observed
-// Needs to stop work when not observed
-// Allow for forced updates to clear out decrypted?
